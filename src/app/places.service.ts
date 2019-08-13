d91b2483fe2509bb5f70d3c063d36438c3d98cbd
Thaler <Roman.Thaler@adesso.at>
2019-08-13 16:10:20 +0200

    added more destinations

--- src/app/places.service.ts
+++ src/app/places.service.ts
@@ -8,11 +8,15 @@ export class PlacesService {
 
   places: Place[] = [
     // tslint:disable-next-line: max-line-length
-    {placeName: 'Vienna', placeRegion: 'Vienna', placeCountry:'Austria', placeVisited:'Mar 12 2019 10:00:00 AM', placeImageUrl: 'https://lonelyplanetimages.imgix.net/mastheads/stock-photo-st-stephens-church-112868985.jpg?sharp=10&vib=20&w=1200'},
+    {placeName: 'Vienna', placeRegion: 'Vienna', placeCountry:'Austria', placeVisited:'Jan 02 2019 10:00:00 AM', placeImageUrl: 'https://lonelyplanetimages.imgix.net/mastheads/stock-photo-st-stephens-church-112868985.jpg?sharp=10&vib=20&w=1200'},
     // tslint:disable-next-line: max-line-length
-    {placeName: 'New York City', placeRegion: 'New York', placeCountry:'USA', placeVisited:'Mar 12 2020 10:00:00 AM', placeImageUrl: 'https://pix10.agoda.net/geo/city/318/1_318_02.jpg?s=1920x822'},
+    {placeName: 'New York City', placeRegion: 'New York', placeCountry:'USA', placeVisited:'Sep 18 2020 10:00:00 AM', placeImageUrl: 'https://pix10.agoda.net/geo/city/318/1_318_02.jpg?s=1920x822'},
     // tslint:disable-next-line: max-line-length
-    {placeName: 'Biarritz', placeRegion: 'Biarritz', placeCountry:'France', placeVisited:'Mar 12 2001 10:00:00 AM', placeImageUrl: 'https://static.geo.de/bilder/eb/58/81806/article_image_big/biarritz-m-mxpnhw.jpg'}
+    {placeName: 'Biarritz', placeRegion: 'Biarritz', placeCountry:'France', placeVisited:'Mar 12 2001 10:00:00 AM', placeImageUrl: 'https://static.geo.de/bilder/eb/58/81806/article_image_big/biarritz-m-mxpnhw.jpg'},
+    // tslint:disable-next-line: max-line-length
+    {placeName: 'London', placeRegion: 'England', placeCountry:'United Kingdom', placeVisited:'Dec 03 2004 10:00:00 AM', placeImageUrl: 'https://www.visitbritain.com/sites/default/files/styles/consumer_vertical_hero__1920x1080/public/paragraphs_bundles/campaign_offer/images/london_0.jpg?itok=1vGDckSM'},
+    // tslint:disable-next-line: max-line-length
+    {placeName: 'Ubud', placeRegion: 'Bali', placeCountry:'Indonesia', placeVisited:'Aug 25 2018 10:00:00 AM', placeImageUrl: 'http://static.asiawebdirect.com/m/bangkok/portals/bali-indonesia-com/homepage/magazine/reasons-ubud-better-than-beach/pagePropertiesImage/ubud.jpg.jpg'}
   ]
   constructor() { }
 
